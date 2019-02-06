1bb547358a6255be411a547a3dd767ab3533508e
Pat  Pignataro <pat6284@msn.com>
2019-02-06 18:16:28 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -5,7 +5,7 @@ export default class Comment extends React.Component {
   render() {
     return (
       <div className="comment">
-      {this.props.commenttext}</div> 
+      {this.props.commentText}</div> 
     )
   }
 }  
