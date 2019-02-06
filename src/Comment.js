f6f63a180f9eecfac24acf7b0e2633592d3a950f
Pat  Pignataro <pat6284@msn.com>
2019-02-06 18:08:54 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,11 @@
-// add Comment component here
+import React, { Component } from 'react';
+import Blogpost from './Blogpost';
+
+class Comment extends Conmponent {
+  render() {
+    return (
+      <div className="comment"></div> 
+    )
+  }
+  
+  export default Comment;
